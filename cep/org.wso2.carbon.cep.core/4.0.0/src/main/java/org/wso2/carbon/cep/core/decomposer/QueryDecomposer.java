9dc0816366ba3c4c9a6b34ad79443a363c36fd43
sachini <sachinijayasekara.g@gmail.com>
2013-06-13 11:26:26 +0530

    implement ip list functionality siddhi api changes

---
+++ cep/org.wso2.carbon.cep.core/4.0.0/src/main/java/org/wso2/carbon/cep/core/decomposer/QueryDecomposer.java
@@ -0,0 +1,21 @@
+package org.wso2.carbon.cep.core.decomposer;
+
+import org.wso2.carbon.cep.core.Query;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sachini
+ * Date: 6/13/13
+ * Time: 10:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface QueryDecomposer {
+
+
+    /**
+    * This method is used to decompose a complex query into small sub queries
+    * @param query - query to be decomposed
+    * */
+    public void decompose(Query query) ;
+
+}
