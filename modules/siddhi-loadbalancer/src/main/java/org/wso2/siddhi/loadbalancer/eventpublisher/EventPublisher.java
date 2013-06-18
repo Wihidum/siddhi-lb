17ca3f6e95cf079fdc8ea4369a29338ed4802204
isuru <irjanith@gmail.com>
2013-06-18 15:36:37 +0530

    eventpublisher

--- modules/siddhi-loadbalancer/src/main/java/org/wso2/siddhi/loadbalancer/eventpublisher/EventPublisher.java
+++ modules/siddhi-loadbalancer/src/main/java/org/wso2/siddhi/loadbalancer/eventpublisher/EventPublisher.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 
 
 import org.wso2.carbon.databridge.commons.Event;
+import org.wso2.carbon.databridge.commons.exception.NoStreamDefinitionExistException;
+import org.wso2.carbon.databridge.commons.exception.StreamDefinitionException;
 import org.wso2.siddhi.loadbalancer.utils.KeyStoreUtil;
 import org.wso2.carbon.databridge.agent.thrift.Agent;
 import org.wso2.carbon.databridge.agent.thrift.DataPublisher;
@@ -14,6 +16,8 @@ import java.util.List;
 
 public class EventPublisher {
     private static Logger logger = Logger.getLogger(EventPublisher.class);
+    private static  String STREAM_NAME="org.wso2.phone.retail.store" ;
+    private static  String VERSION= "1.2.0";
 
     public static void publishEvents(String host,String port,List<Event> eventList)
     {
@@ -27,10 +31,24 @@ public class EventPublisher {
             e.printStackTrace();
             dataPublisher.stop();
         }
+        String streamId=null;
+
+            try {
+               streamId = dataPublisher.findStream(STREAM_NAME,VERSION);
+            } catch (AgentException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (StreamDefinitionException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (NoStreamDefinitionExistException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            //System.out.println("Stream already defined");
+
+
 
         for(Event event:eventList)  {
             try {
-                dataPublisher.publish(event);
+                dataPublisher.publish(streamId,event.getMetaData(),event.getCorrelationData(),event.getPayloadData());
                 // logger.info("publishEvent");
             } catch (AgentException e) {
                 e.printStackTrace();
